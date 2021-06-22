4cbd609e9bb92bfef0e792f5e576bf7dd683de1b
Antony <ac.chan@live.com>
2021-06-22 00:40:45 +0000

    implement head

--- head.js
+++ head.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
   } else {
@@ -6,9 +6,9 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const head = function (arr) {
+const head = function(arr) {
   return arr[0];
-}
+};
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
