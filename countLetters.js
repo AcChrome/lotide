f05e2bc9796afab63aa58014c2073678abdfceb5
Antony <ac.chan@live.com>
2021-06-24 01:58:51 +0000

    countLetters

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed; [${actual}] !== [${expected}]`);
+  }
+};
+
+
+const countLetters = function (str) {
+  let result = {};
+  let newStr = str.split(" ").join("");
+  
+  for (let letter of newStr) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+
+console.log(countLetters('LHL'))
+console.log(countLetters("lighthouse in the house"));
