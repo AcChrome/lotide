2fda7a13e7f45f3f68bb8ca427decae0675d782b
Antony <ac.chan@live.com>
2021-06-29 09:50:32 +0000

    index update

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const countLetters = require('./countLetters');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findkeyByValue = require('./findkeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  countLetters: countLetters,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findkeyByValue: findkeyByValue,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil
+
+
 };
